3f7b8d1d31248e4a5f22db6310b81be01ca25241
Matt Parsons <parsonsmatt@gmail.com>
2016-06-25 03:13:21 -0400

    cats takin over

--- src/Api.hs
+++ src/Api.hs
@@ -1,23 +1,38 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators   #-}
+
 module Api where
 
+import Data.Aeson
 import Network.HTTP.ReverseProxy
 import Network.HTTP.Client (Manager, defaultManagerSettings, newManager)
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-type API = Raw
+type API
+    = "cat" :> Get '[JSON] Cat
 
-forwardRequest :: Request -> IO WaiProxyResponse
-forwardRequest _ = 
-    pure . WPRProxyDest . ProxyDest "127.0.0.1" $ 4567
+newtype Cat = Cat { cat :: String }
+
+instance ToJSON Cat where
+    toJSON (Cat mew) =
+        object [ "cat" .= mew ]
+
+server :: Server API
+server = pure (Cat { cat = "mrowl" })
+
+api :: Proxy (API :<|> Raw)
+api = Proxy
 
 app :: Manager -> Application
 app manager =
-    waiProxyTo forwardRequest defaultOnExc manager
+    serve api $ server :<|> waiProxyTo forwardRequest defaultOnExc manager
+
+forwardRequest :: Request -> IO WaiProxyResponse
+forwardRequest _ =
+    pure . WPRProxyDest . ProxyDest "127.0.0.1" $ 4567
 
 startApp :: IO ()
 startApp = do
